8395ffc835b3b66d41d1742e6164fbd00fa5999f
hydravertigo <159677492+hydravertigo@users.noreply.github.com>
2025-03-16 19:28:58 +0000

    initial tests

---
+++ BusShuttleProject/BusShuttle.Tests/FileSaverTests.cs
@@ -0,0 +1,26 @@
+namespace BusShuttle.Tests;
+
+using BusShuttle;
+
+public class FileSaverTests
+{
+    FileSaver fileSaver;
+    string testFileName;
+
+
+    public FileSaverTests() {
+        testFileName = "test-doc.txt";
+        fileSaver = new FileSaver(testFileName);
+    }
+
+    [Fact]
+    public void Test_FileSaver_Append()
+    {
+        fileSaver.AppendLine("Hello, world");
+
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal("Hello, world" + Environment.NewLine, contentFromFile);
+
+
+    }
+}
